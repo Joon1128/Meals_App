6346eb52b47ab74815757cc65c57054ce8839b0d
chohj <chohj@ais-info.co.jp>
2025-07-31 15:58:40 +0900

    added design

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -45,7 +46,7 @@ class MealsScreen extends StatelessWidget {
       content = ListView.builder(
         itemCount: meals.length,
         itemBuilder: (ctx, index) =>
-            Text(meals[index].title), // indexに該当するitemを返却
+            MealItem(meal: meals[index]),
       );
     }
 
